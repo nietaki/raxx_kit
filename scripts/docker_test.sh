39d76778d3db8eb0b92e29a02926568918c9b590
Jacek Królikowski <nietaki@gmail.com>
2018-11-08 21:18:44 +0000

    Start using docker-compose

--- scripts/docker_test.sh
+++ scripts/docker_test.sh
@@ -2,4 +2,20 @@
 
 set -eu
 
-echo "## docker tests"
+echo "## generating a demo project"
+
+mix compile
+mix raxx.kit --docker demo
+pushd demo
+
+echo "## compiling demo project inside the container"
+
+docker-compose run mix compile
+
+echo "## running tests for demo project inside the container"
+
+docker-compose run mix test
+
+echo "## running tests for demo project inside the container"
+
+docker-compose run mix format --check-formatted
